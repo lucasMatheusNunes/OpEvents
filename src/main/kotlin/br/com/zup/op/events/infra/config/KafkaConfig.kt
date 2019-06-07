c03cfdc735cfbb56a368b732fdfd00449c029096
brunoroger <bschiochet@gmail.com>
2019-06-07 16:40:12 -0300

    clean imports config kafka

--- src/main/kotlin/br/com/zup/op/events/infra/config/KafkaConfig.kt
+++ src/main/kotlin/br/com/zup/op/events/infra/config/KafkaConfig.kt
@@ -1,16 +1,9 @@
 package br.com.zup.op.events.infra.config
 
 import org.apache.kafka.clients.consumer.Consumer
-import org.apache.kafka.clients.producer.ProducerConfig
-import org.apache.kafka.common.serialization.StringSerializer
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.core.ConsumerFactory
-import org.springframework.kafka.core.DefaultKafkaProducerFactory
-import org.springframework.kafka.core.KafkaTemplate
-import org.springframework.kafka.core.ProducerFactory
-import java.util.*
 
 @Configuration
 class KafkaConfig(private val consumerFactory: ConsumerFactory<String, Any>) {
