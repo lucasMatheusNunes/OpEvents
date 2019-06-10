28fc6302a8e049379a8e86a7825971104877005c
Lucas Matheus Nunes <lucas.mts75@gmail.com>
2019-06-10 13:17:23 -0300

    Adjust in flyway insets

--- src/main/resources/db/migration/structure/V1__init.sql
+++ src/main/resources/db/migration/structure/V1__init.sql
@@ -1,5 +1,11 @@
-CREATE TABLE topic (
-    name varchar(255) PRIMARY KEY
+CREATE TABLE event (
+    id UUID PRIMARY KEY NOT NULL,
+    topic varchar(255),
+    payload text,
+    reason varchar(255),
+    user_id varchar(255),
+    _key varchar(255),
+    note varchar(255)
 );
 
 CREATE TABLE reason (
